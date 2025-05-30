efd085ad22bf9e084a1de2b29cff0a37d08afcc8
abu munib <aminkafrinewgen@gmail.com>
2025-05-30 12:47:21 +0800

    add undotree

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,6 +26,15 @@ vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 
+vim.opt.hlsearch = false
+
+vim.opt.incsearch = true
+vim.opt.swapfile = false
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "100"
+vim.opt.undofile = true
+vim.opt.signcolumn = "yes"
+
 require("config.remap")
 
 -- Setup lazy.nvim
--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,3 +1,19 @@
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-vim.keymap.set('n', '<leader>x', ':so<cr>')
 
+vim.keymap.set('n', '<leader><leader>x', '<cmd>source %<cr>')
+vim.keymap.set('n', '<leader>x', ':.lua<cr>')
+vim.keymap.set('v', '<leader>x', ':lua<cr>')
+
+vim.keymap.set('i', '<C-c>', '<Esc>');
+--
+vim.keymap.set('n', "<M-j>", '<cmd>cnext<CR>zz')
+vim.keymap.set('n', "<M-k>", '<cmd>cprev<CR>zz')
+
+vim.keymap.set('v', 'J', ":m '>+1'<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2'<CR>gv=gv")
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
