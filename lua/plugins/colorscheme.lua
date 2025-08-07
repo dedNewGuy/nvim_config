b3b4976d212107e468d42647ebbf664a85f4fefe
dedNewGuy <aminkafrinewgen@gmail.com>
2025-08-08 00:24:31 +0800

    add fidget and gitsign

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,16 +8,6 @@ return {
 	},
 
 	{
-		'daneofmanythings/chalktone.nvim',
-		priority = 1000,
-		lazy = false,
-		config = function()
-			require('chalktone').setup()
-		end
-	},
-
-	{ "blazkowolf/gruber-darker.nvim" },
-	{
 		"rose-pine/neovim",
 		name = "rose-pine",
 		config = function ()
